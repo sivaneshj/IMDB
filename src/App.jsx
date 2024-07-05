c1dae58cc2f7aa7939722ff64e0a703b7f002266
sivaneshj <sivaneshwaran03@gmail.com>
2024-07-05 12:24:34 +0530

    code modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,66 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Navbar from "./component/Navbar"
-import Watchlist from './component/watchlist'
-import Movies from './component/movies'
-import Banner from "./component/banner"
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Watchlist from "./component/watchlist";
+import Movies from "./component/movies";
+import Banner from "./component/banner";
 function App() {
-  var [wathlistmovies,addwm] =  useState([]);
-  
-  var addtowatch=(movobj)=>{
-    if(wathlistmovies.indexOf(movobj) == -1){
-      let totalmov = [...wathlistmovies,movobj];
-      localStorage.setItem("moviesdata",JSON.stringify(totalmov));
+  var [wathlistmovies, addwm] = useState([]);
+
+  var addtowatch = (movobj) => {
+    if (wathlistmovies.indexOf(movobj) == -1) {
+      let totalmov = [...wathlistmovies, movobj];
+      localStorage.setItem("moviesdata", JSON.stringify(totalmov));
       addwm([...new Set(totalmov)]);
     }
-
-    
-  }
+  };
   console.log(wathlistmovies);
 
-  var deletemovie=(movdel)=>{
-    var remin = wathlistmovies.filter((x)=>{
+  var deletemovie = (movdel) => {
+    var remin = wathlistmovies.filter((x) => {
       return x.id != movdel.id;
     });
-    localStorage.setItem("moviesdata",JSON.stringify(remin));
+    localStorage.setItem("moviesdata", JSON.stringify(remin));
     addwm(remin);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     let storage = JSON.parse(localStorage.getItem("moviesdata"));
     addwm(storage);
-  },[])
+  }, []);
 
   return (
     <>
-     <BrowserRouter>
-     <Navbar/>
-     <Routes>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <Movies
+                  deletemovie={deletemovie}
+                  watchlistmovies={wathlistmovies}
+                  addtowatch={addtowatch}
+                />
+              </>
+            }
+          />
 
-      <Route path='/' element={<><Banner/><Movies deletemovie={deletemovie} watchlistmovies={wathlistmovies} addtowatch={addtowatch}/></>}/>
-
-      <Route path='/watchlist' element={<Watchlist addwm={addwm} wathlistmovies={wathlistmovies}/>}/>
-
-     </Routes>
-
-     </BrowserRouter>
-     
+          <Route
+            path="/watchlist"
+            element={
+              <Watchlist addwm={addwm} wathlistmovies={wathlistmovies} />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/watchlist.jsx
+++ src/component/watchlist.jsx
@@ -1,36 +1,53 @@
 import React, { useState } from "react";
 
-const watchlist = ({ wathlistmovies,addwm }) => {
+const watchlist = ({ wathlistmovies, addwm }) => {
+  var [search, addsearch] = useState("");
+  function deletemovie(movieObj) {
+    let movielist = wathlistmovies.filter((x) => {
+      return x.id != movieObj.id;
+    });
+    localStorage.setItem("moviesdata", JSON.stringify(movielist));
+    addwm(movielist);
+  }
 
-  var [search,addsearch] = useState('');
-function deletemovie(movieObj){
-  let movielist = wathlistmovies.filter((x)=>{
-    return x.id != movieObj.id;
-  });
-  localStorage.setItem("moviesdata",JSON.stringify(movielist));
-  addwm(movielist);
-}
+  function ascend() {
+    let descen = wathlistmovies.sort((a, b) => {
+      return b.vote_average - a.vote_average;
+    });
+    addwm([...descen]);
+  }
 
-  function handlesearch(e){
+  function descend() {
+    let ascen = wathlistmovies.sort((a, b) => {
+      return a.vote_average - b.vote_average;
+    });
+    addwm([...ascen]);
+  }
+  function normal() {
+    let defaults = JSON.parse(localStorage.getItem("moviesdata"));
+    addwm([...defaults]);
+  }
+
+  function handlesearch(e) {
     addsearch(e.target.value);
   }
   return (
-      <div className="bg-gray-900 h-[86vh]">
+    <div className="bg-gray-900 h-[86vh]">
       <div className="flex  justify-center gap-2 py-4">
         <div className="rounded-xl text-white bg-blue-400 px-3 py-2">
           Action
         </div>
-        <div className="rounded-xl text-white bg-gray-400 px-3 py-2">
+        <div className="rounded-xl text-white bg-green-400 px-3 py-2">
           Action
         </div>
-        <div className="rounded-xl text-white bg-gray-400 px-3 py-2">
+        <div className="rounded-xl text-white bg-red-400 px-3 py-2">
           Action
         </div>
       </div>
       <div className="text-center ">
         <input
-        onChange={handlesearch}
-        value={search}
+          onChange={handlesearch}
+          value={search}
           className="rounded-lg bg-gray-400/50 text-white outline-none p-2 my-3 h-[2.5rem] w-[15rem]"
           placeholder="Search movies"
           type="text"
@@ -40,53 +57,103 @@ function deletemovie(movieObj){
       <div>
         <table className="w-full border-gray-400  mt-3">
           <thead className="text-center text-white border-2">
-            <tr>
+            <tr className=''>
               <th className="">Poster</th>
               <th className="">Name</th>
-              <th>Ratings</th>
+              <th>
+                <div className="flex py-3 items-center justify-center gap-2  text-white-900">
+                  <div className="hover:cursor-pointer" onClick={ascend}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-up"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
+                      />
+                    </svg>
+                  </div>
+                  <div className="hover:cursor-pointer" onClick={normal}>
+                  Ratings
+                  </div>
+                  <div className="hover:cursor-pointer" onClick={descend}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-down"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </th>
               <th>Popular</th>
               <th>Genre</th>
             </tr>
           </thead>
 
           <tbody className="text-white border-2 ">
-            {wathlistmovies.filter((movieObj)=>{
-              return movieObj.original_title.toLowerCase().includes(search.toLocaleLowerCase())
-            }).map((movieObj) => {
-              console.log(movieObj);
-              return (
-                <>
-                  <tr className="border-b-2 mb-5">
-                    <td className="w-[8rem] h-[18vh]">
-                      <img
-                        className=" h-full w-full p-2"
-                        src={
-                          `https://image.tmdb.org/t/p/original${movieObj.poster_path}`
-                        }
-                        alt=""
-                      />
-                    </td>
+            {wathlistmovies
+              .filter((movieObj) => {
+                return movieObj.original_title
+                  .toLowerCase()
+                  .includes(search.toLocaleLowerCase());
+              })
+              .map((movieObj) => {
+                console.log(movieObj);
+                return (
+                  <>
+                    <tr className="border-b-2 mb-5">
+                      <td className="w-[8rem] h-[18vh]">
+                        <img
+                          className=" h-full w-full p-2"
+                          src={`https://image.tmdb.org/t/p/original${movieObj.poster_path}`}
+                          alt=""
+                        />
+                      </td>
 
-                    <td className=" text-center p-2  w-[30rem]">{movieObj.original_title}</td>
+                      <td className=" text-center p-2  w-[30rem]">
+                        {movieObj.original_title}
+                      </td>
 
-                    <td className="text-center p-2 w-[10rem]">{movieObj.vote_average}</td>
+                      <td className="text-center p-2 w-[10rem]">
+                        {movieObj.vote_average}
+                      </td>
 
-                    <td className="text-center p-2 w-[10rem]">{movieObj.vote_count}</td>
+                      <td className="text-center p-2 w-[10rem]">
+                        {movieObj.vote_count}
+                      </td>
 
-                    <td className="text-center p-2 w-[10rem]">Action</td>
+                      <td className="text-center p-2 w-[10rem]">Action</td>
 
-                    <td className="w-[8rem] text-center">
-                      
-                      <h2 onClick={()=>deletemovie(movieObj)} className="text-red-600 hover:cursor-pointer font-bold  p-2">Delete</h2>
-                    </td>
-                  </tr>
-                </>
-              );
-            })}
+                      <td className="w-[8rem] h-[18vh] text-center  justify-center">
+                        <div className="h-full flex justify-center items-center">
+                          <h2
+                            onClick={() => deletemovie(movieObj)}
+                            className="text-white bg-red-600 w-[5rem]  hover:cursor-pointer hover:bg-blue-400 font-bold rounded-lg  py-1"
+                          >
+                            Delete
+                          </h2>
+                        </div>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
           </tbody>
         </table>
       </div>
-      </div>
+    </div>
   );
 };
 
